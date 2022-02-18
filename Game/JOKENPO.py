028a1edc889643c1cee0c82301d3f240d0f21ef1
Ahamatell Yagura Khamatsutt <yaguraKhamatsutt@gmail.com>
2022-02-18 18:55:43 -0300

    End of the first part

--- Game/JOKENPO.py
+++ Game/JOKENPO.py
@@ -3,6 +3,9 @@ from time import sleep
 
 print('\33[33m{:=^60}' .format('JO KEN PO'))
 
+opponentWillPlay = randint(0, 2)
+itens = ['ROCK', 'PAPER', 'SCISSORS']
+
 print('''\33[34mChoose the following options
 [0] ROCK 
 [1] PAPER
@@ -12,42 +15,40 @@ print('\33[34m=-' * 30)
 
 player = int(input('\33[36mWhich will you choose?'))
 
-iA = randint(0, 2)
-intens = [0, 1, 2]
+print('\33[33m Opponent will play...')
 
-print('\33[33m opponent will play...')
-
-
-print('JO')
+print('\33[32mJO')
 sleep(1)
-print('KEN')
+print('\33[34mKEN')
 sleep(1)
-print('PO!!')
+print('\33[36mPO!!')
 sleep(0.5)
-print(intens)
 
-if iA == 0:
+print('You chose {}'.format(itens[player]))
+print('Opponent will play {}'.format(itens[opponentWillPlay]))
+
+if opponentWillPlay == 0:
     if player == 0:
         print('A TIE')
     elif player == 1:
-        print('IT WON')
+        print('\33[34mYOU WIN')
     elif player == 2:
         print('\33[31mGAME OVER')
     else:
         print('\33[31m//ERROR [number invalid!]')
-elif iA == 1:
+elif opponentWillPlay == 1:
     if player == 0:
         print('\33[31mGAME OVER')
     elif player == 1:
         print('A TIE')
     elif player == 2:
-        print('IT WON')
+        print('\33[34mYOU WIN')
     else:
         print('\33[31m//ERROR [number invalid!]')
 
-elif iA == 2:
+elif opponentWillPlay == 2:
     if player == 0:
-        print('IT WON')
+        print('\33[34mYOU WIN')
     elif player == 1:
         print('\33[31mGAME OVER')
     elif player == 2:
